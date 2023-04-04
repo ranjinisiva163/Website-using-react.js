32459bc24cb8ac245976651fcff19143348abd3b
ranjinisiva163 <129645878+ranjinisiva163@users.noreply.github.com>
2023-04-04 19:36:12 +0530

    React. Js

---
+++ Website using react.js
@@ -0,0 +1,48 @@
+HISTORY:
+
+• React was created by Jordan Walke, a software engineer at Meta, who released an early prototype of React called "FaxJS".
+
+• He was influenced by XHP, an HTML component library for PHP. It was first deployed on Facebook's News Feed in 2011 and later on Instagram in 2012.
+
+LIST OF REACT LIBRARIES:
+
+• React router
+• Create-react-app
+• Ant-design
+• Tailwind UI
+• Semantic UI react 
+• React bootstrap
+• Fabric react
+• Styled components
+• React DnD
+• Onsen UI
+• Rebass
+• Material UI
+• React-motion 
+• React spinner
+ADVANTAGES:
+
+• It is composable, It is declarative.
+• Write once, and leam anywhere. 
+• It is simple.
+• SEO friendly.
+• Fast, efficient, and easy to learn.
+• It guarantees stable code.
+• It is backed by a strong community.
+APPLICATIONS:
+
+• Snapchat
+• Instagram 
+• Facebook
+• Yahoo
+• Myntra
+• Netflix
+• Dropbox
+•Snapchat
+LIMITATIONS:
+
+• ReactJS components are difficult to reuse in complex interactive web projects.
+• ReactJS's Virtual DOM algorithm is time-consuming and imprecise.
+• ReactJS's HTML templates are neither complete nor powerful.
+• ReactJS requires complicated asynchronous programming while communicating with the server.
+
